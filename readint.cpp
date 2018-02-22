3ece8b0bb6b64061d9569a691336626589e1072e
Anujin Munkhbat <anujin.munhbat@gmail.com>
2018-02-21 22:43:13 -0800

    Update readint.cpp

--- readint.cpp
+++ readint.cpp
@@ -8,33 +8,30 @@ using namespace std;
 int read_int(const string &prompt, int low, int high)
 {
 	cin.exceptions(ios_base::failbit);
-	int num = 0;
-	try {
-		if (low >= high || high - low == 1) {
-			throw range_error("Exception- The range is invalid!\n");
-		}
-		while (true) { // Loop until valid input
-			try {
-				cout << prompt;
-				cin >> num;
-				while (num <= low || num >= high) {
-					cout << "Value is outside the range. Please try again, ";
-					cin >> num;
-				}
-				return num;
-			}
-			catch (ios_base::failure& ex) {
-				cout << "Bad numeric string -- try again\n";
-				// Reset the error flag
-				cin.clear();
-				// Skip current input line
-				cin.ignore(numeric_limits<int>::max(), '\n');
-
-			}
-		}
+	if (low >= high || high - low == 1) {
+		throw invalid_argument("Exception- The range is invalid!\n");
 	}
-	catch (exception &test) {
-		cerr << " caught: " << test.what() << endl;
-	};
+	int num = 0;
+	while (true) { // Loop until valid input
+		try {
+			cout << prompt;
+			cin >> num;
+			while (num <= low || num >= high) {
+				cout << "Value is outside the range. Please try again, ";
+				cin >> num;
+			}
+			return num;
+		}
+		catch (ios_base::failure& ex) {
+			cout << "Bad numeric string -- try again\n";
+			// Reset the error flag
+			cin.clear();
+			// Skip current input line
+			cin.ignore(numeric_limits<int>::max(), '\n');
+		}
+		catch (exception &test) {
+			cerr << " caught: " << test.what() << endl;
+		}
+	}	
 
 }
